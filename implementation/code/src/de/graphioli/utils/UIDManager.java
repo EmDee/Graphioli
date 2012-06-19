38d14d44a3949b0cc842d3a5ad2a948c840b7636
Johannes Schäfer <github@johannes-schaefer.net>
2012-06-19 14:31:12 +0200

    Implemented UIDManager.

--- implementation/code/src/de/graphioli/utils/UIDManager.java
+++ implementation/code/src/de/graphioli/utils/UIDManager.java
@@ -1,4 +1,5 @@
 package de.graphioli.utils;
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -9,21 +10,52 @@ import java.util.UUID;
 public class UIDManager {
 
 	/**
-	 * List of all generated uuids.
+	 * List of all generated unique IDs.
 	 */
-	private Iterable<UUID> uuids;
+	private static ArrayList<UUID> uuids = new ArrayList<UUID>();
 
 
 	public UIDManager() {}
 
 
+	/**
+	 * Generates a unique ID.
+	 * 
+	 * @return the generated unique ID
+	 */
 	public static UUID generateUniqueID() {
-		
+
+		UUID uuid;
+
+		// Generate new unique ID and ensure uniqueness
+		do {
+			uuid = UUID.randomUUID();
+		} while(!isUnique(uuid));
+
+		// Add unique ID to list of generated IDs
+		uuids.add(uuid);
+
+		return uuid;
+
 	}
 
 
+	/**
+	 * Helper method, which ensures that a generated ID is really unique.
+	 * 
+	 * @param uuid The generated ID to check for uniqueness
+	 * @return <code>true</code> if the ID is really unique, <code>false</code> otherwise
+	 */
 	private static boolean isUnique(UUID uuid) {
-		
+
+		for (UUID currentUuid : uuids) {
+			if (currentUuid.equals(uuid)) {
+				return false;
+			}
+		}
+
+		return true;
+
 	}
 
 }
