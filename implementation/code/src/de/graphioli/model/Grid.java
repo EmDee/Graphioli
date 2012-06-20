ee18846f0cbe3841eacfda8149a15f7c8c82dd47
Johannes Schäfer <github@johannes-schaefer.net>
2012-06-20 14:17:57 +0200

    Implemented Grid and GridPoint.

--- implementation/code/src/de/graphioli/model/Grid.java
+++ implementation/code/src/de/graphioli/model/Grid.java
@@ -1,5 +1,105 @@
 package de.graphioli.model;
 
+/**
+ * This class represents the grid, on which the {@link Graph} will be located.
+ * 
+ * @author Graphioli
+ * @todo Implement ArrayIndexOutOfBounds checkings
+ */
 public class Grid {
+
+	/**
+	 * The number of horizontal grid points
+	 */
+	private int horizontalGridPoints;
+
+	/**
+	 * The number of vertical grid points
+	 */
+	private int verticalGridPoints;
+
+	/**
+	 * Array-based representation of the grid
+	 */
+	private VisualVertex[][] grid;
+
+
+	/**
+	 * Creates a new {@link Grid} with the specified parameters.
+	 * 
+	 * @param horizontalGridPoints The number of horizontal grid points
+	 * @param verticalGridPoints The number of vertical grid points
+	 */
+	public Grid(int horizontalGridPoints, int verticalGridPoints) {
+
+		this.horizontalGridPoints = horizontalGridPoints;
+		this.verticalGridPoints = verticalGridPoints;
+		this.grid = new VisualVertex[horizontalGridPoints][verticalGridPoints];
+
+	}
+
+
+	/**
+	 * Adds the specified {@link VisualVertex} to the Grid.
+	 * 
+	 * @param visualVertex The VisualVertex to add
+	 * @return <code>true</code> if the VisualVertex was added successfully to this Grid, <code>false</code> otherwise
+	 */
+	public boolean addVisualVertexToGrid(GridPoint gridPoint, VisualVertex visualVertex) {
+
+		// Get VisualVertex at specified GridPoint
+		VisualVertex visualVertexAtGridPoint = this.grid[gridPoint.getPositionX()][gridPoint.getPositionY()];
+
+		// Return false if GridPoint is not empty
+		if (visualVertexAtGridPoint != null) {
+			return false;
+		}
+
+		// Add VisualVertex
+		this.grid[gridPoint.getPositionX()][gridPoint.getPositionY()] = visualVertex;
+
+		return true;
+
+	}
+
+
+	/**
+	 * Removes the {@link VisualVertex} from the specified {@link GridPoint} on the Grid.
+	 * 
+	 * @param gridPoint The GridPoint from which the VisualVertex will be removed
+	 * @return <code>true</code> if a VisualVertex was removed from this GridPoint on the Grid, <code>false</code> otherwise
+	 */
+	public boolean removeVisualVertexAtGridPoint(GridPoint gridPoint) {
+
+		// Get VisualVertex at specified GridPoint
+		VisualVertex visualVertex = this.grid[gridPoint.getPositionX()][gridPoint.getPositionY()];
+
+		// Return false if GridPoint is empty
+		if (visualVertex == null) {
+			return false;
+		}
+
+		// Remove VisualVertex
+		this.grid[gridPoint.getPositionX()][gridPoint.getPositionY()] = null;
+
+		return true;
+
+	}
+
+
+	/**
+	 * Returns the {@link VisualVertex} at the specified {@link GridPoint}.
+	 * 
+	 * @param gridPoint the GridPoint of the VisualVertex to return
+	 * @return the VisualVertex at the specified GridPoint or <code>null</code> if the GridPoint is empty
+	 */
+	public VisualVertex getVisualVertexAtGridPoint(GridPoint gridPoint) {
+
+		// Get VisualVertex at specified GridPoint
+		VisualVertex visualVertex = this.grid[gridPoint.getPositionX()][gridPoint.getPositionY()];
+
+		return visualVertex;
+
+	}
 
 }
--- implementation/code/src/de/graphioli/model/GridPoint.java
+++ implementation/code/src/de/graphioli/model/GridPoint.java
@@ -1,5 +1,51 @@
 package de.graphioli.model;
 
+/**
+ * This class represents the grid, on which the {@link Graph} will be located.
+ * 
+ * @author Graphioli
+ * @todo Implement ValidParameters checkings
+ */
 public class GridPoint {
+
+	/**
+	 * The x coordinate of the GridPoint
+	 */
+	private int positionX;
+
+	/**
+	 * The y coordinate of the GridPoint
+	 */
+	private int positionY;
+
+
+	/**
+	 * Creates a new GridPoint with the specified positions positionX and positionY.
+	 * 
+	 * @param positionX The x coordinate of the GridPoint
+	 * @param positionY The y coordinate of the GridPoint
+	 */
+	GridPoint(int positionX, int positionY) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+
+	/**
+	 * Returns the x coordinate of the {@link GridPoint}.
+	 * @return the x coordinate of the GridPoint
+	 */
+	public int getPositionX() {
+		return this.positionX;
+	}
+
+
+	/**
+	 * Returns the y coordinate of the {@link GridPoint}.
+	 * @return the y coordinate of the GridPoint
+	 */
+	public int getPositionY() {
+		return this.positionY;
+	}
 
 }
