f565448b3732a4cf540dc960f98871b4a44a1199
Martin Dinh <martin@dinhmail.de>
2012-06-21 03:12:20 +0200

    partially implemented VisualVertex

--- implementation/code/src/de/graphioli/model/VisualVertex.java
+++ implementation/code/src/de/graphioli/model/VisualVertex.java
@@ -1,5 +1,37 @@
 package de.graphioli.model;
 
-public abstract class VisualVertex extends Vertex {
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
+public abstract class VisualVertex extends Vertex {
+	private GridPoint gridPoint;
+	protected BufferedImage bufferedImage;
+	
+	public VisualVertex(GridPoint gridPoint) {
+		this.gridPoint = gridPoint;
+	}
+	
+	protected abstract boolean draw(Graphics2D graphics);
+
+	public final boolean update() {
+		return true;
+	}
+	
+	public final BufferedImage getBufferedImage() {
+		return this.bufferedImage;
+	}
+
+	/**
+	 * @return the gridPoint
+	 */
+	public GridPoint getGridPoint() {
+		return gridPoint;
+	}
+
+	/**
+	 * @param gridPoint the gridPoint to set
+	 */
+	public void setGridPoint(GridPoint gridPoint) {
+		this.gridPoint = gridPoint;
+	}
 }
