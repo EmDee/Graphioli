d19ccdefcbb3c61e870d53c58584c6868eb4ef08
Martin Dinh <martin@dinhmail.de>
2012-06-22 11:50:23 +0200

    initialized part structure of jUnit test environment

---
+++ implementation/code/test/de/graphioli/model/GridTest.java
@@ -0,0 +1,29 @@
+package de.graphioli.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridTest {
+
+	@Test
+	public void testGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddVisualVertexToGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveVisualVertexAtGridPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVisualVertexAtGridPoint() {
+		fail("Not yet implemented");
+	}
+
+}
