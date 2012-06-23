80c5e624e736a05fec5369f415f3fa0d5b06dc90
Leon Hielscher <Leon@dr-hielscher.de>
2012-06-23 19:40:29 +0200

    Implemented VisualVertex and SimpleVisualVertex. Not tested, yet!

--- implementation/code/src/de/graphioli/model/SimpleVisualVertex.java
+++ implementation/code/src/de/graphioli/model/SimpleVisualVertex.java
@@ -1,18 +1,99 @@
 package de.graphioli.model;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 public class SimpleVisualVertex extends VisualVertex {
 
+	private Color fillColor;
+	private Color strokeColor;
+	private int strokeWeight;
+
+	private Stroke stroke;
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public SimpleVisualVertex(GridPoint gridPoint) {
 		super(gridPoint);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Sets the fill color of this {@code SimpleVisualVertex} to {@code WHITE},
+	 * the stroke color to {@code BLACK} and the stroke weight to {@code 1}.
+	 */
+
+	@Override
+	protected void init() {
+		fillColor = Color.WHITE;
+		strokeColor = Color.BLACK;
+		strokeWeight = 1;
+	}
+
+	/**
+	 * Draws this {@code SimpleVisualVertex} according to its fill color, stroke
+	 * color and stroke weight.
+	 * 
+	 */
 	@Override
 	protected boolean draw(Graphics2D graphics) {
-		// TODO Auto-generated method stub
-		return false;
+		graphics.setStroke(stroke);
+		graphics.setColor(fillColor);
+		graphics.fillOval(0, 0, 74, 74);
+		graphics.setColor(strokeColor);
+		graphics.drawOval(0, 0, 74, 74);
+		return true;
+	}
+
+	/**
+	 * @return the fillColor
+	 */
+	public Color getFillColor() {
+		return fillColor;
+	}
+
+	/**
+	 * @param fillColor
+	 *            the fillColor to set
+	 */
+	public void setFillColor(Color fillColor) {
+		this.fillColor = fillColor;
+		this.update();
+	}
+
+	/**
+	 * @return the strokeColor
+	 */
+	public Color getStrokeColor() {
+		return strokeColor;
+	}
+
+	/**
+	 * @param strokeColor
+	 *            the strokeColor to set
+	 */
+	public void setStrokeColor(Color strokeColor) {
+		this.strokeColor = strokeColor;
+		this.update();
+	}
+
+	/**
+	 * @return the strokeWeight
+	 */
+	public int getStrokeWeight() {
+		return strokeWeight;
+	}
+
+	/**
+	 * @param strokeWeight
+	 *            the strokeWeight to set
+	 */
+	public void setStrokeWeight(int strokeWeight) {
+		this.strokeWeight = strokeWeight;
+		this.stroke = new BasicStroke(strokeWeight);
+		this.update();
 	}
 
 }
--- implementation/code/src/de/graphioli/model/VisualVertex.java
+++ implementation/code/src/de/graphioli/model/VisualVertex.java
@@ -1,37 +1,115 @@
 package de.graphioli.model;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.util.logging.Logger;
 
 public abstract class VisualVertex extends Vertex {
+
+	private final static Logger LOG = Logger.getLogger(VisualVertex.class
+			.getName());
+
 	private GridPoint gridPoint;
 	protected BufferedImage bufferedImage;
-	
+
+	/**
+	 * The length of a side in pixels.
+	 */
+	public static final int PIXELS_PER_SIDE = 75;
+
+	/**
+	 * Constructs a {@code VisualVertex} with the given {@link GridPoint},
+	 * initializes and updates it.
+	 * 
+	 * @param gridPoint
+	 *            the {@link GridPoint} for the new vertex.
+	 */
 	public VisualVertex(GridPoint gridPoint) {
 		this.gridPoint = gridPoint;
+		bufferedImage = new BufferedImage(PIXELS_PER_SIDE, PIXELS_PER_SIDE,
+				BufferedImage.TYPE_4BYTE_ABGR);
+		init();
+		update();
 	}
-	
+
+	/**
+	 * Initialize this {@code VisualVertex}.
+	 */
+	protected void init() {
+	}
+
+	/**
+	 * Draws the visualization of this {@code VisualVertex} onto the given
+	 * {@link Graphics2D} instance.
+	 * 
+	 * @param graphics
+	 *            the graphic to draw on.
+	 * @return {@code true} when drawing was successful.
+	 */
 	protected abstract boolean draw(Graphics2D graphics);
 
-	public final boolean update() {
-		return true;
+	/**
+	 * Recreates the buffered image of this {@code VisualVertex}. Has to be
+	 * called to change the visualization.
+	 * 
+	 * @return {@code true} when updating was successful.
+	 */
+	protected final boolean update() {
+
+		LOG.finer("Updating VisualVertex at position " + getGridPoint() + ".");
+
+		clearBufferedImage();
+		Graphics2D g2d = bufferedImage.createGraphics();
+
+		// Use anti-aliasing
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		return draw(g2d);
 	}
-	
+
+	/**
+	 * Returns the graphic representation of this {@code VisualVertex}.
+	 * 
+	 * 
+	 * @return the graphic representation.
+	 */
 	public final BufferedImage getBufferedImage() {
+
 		return this.bufferedImage;
 	}
 
 	/**
-	 * @return the gridPoint
+	 * Returns the {@link GridPoint} of this {@code VisualVertex}.
+	 * 
+	 * @return the grid point.
 	 */
 	public GridPoint getGridPoint() {
 		return gridPoint;
 	}
 
 	/**
-	 * @param gridPoint the gridPoint to set
+	 * 
+	 * @param gridPoint
+	 *            the gridPoint to set
 	 */
 	public void setGridPoint(GridPoint gridPoint) {
+		// TODO: Take out this method?
+		LOG.warning("Resetting the position of a VisualVertex from "
+				+ this.gridPoint + " to " + gridPoint
+				+ ". This might lead to inconsistencies!");
 		this.gridPoint = gridPoint;
+	}
+
+	/**
+	 * Resets the buffered image of this {@code VisualVertex} to a completely
+	 * transparent state.
+	 */
+	private final void clearBufferedImage() {
+		Graphics2D g2d = bufferedImage.createGraphics();
+		g2d.setBackground(new Color(255, 255, 255, 0));
+		g2d.clearRect(0, 0, PIXELS_PER_SIDE, PIXELS_PER_SIDE);
 	}
 }
