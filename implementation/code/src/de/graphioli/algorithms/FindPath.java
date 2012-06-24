0b69de0a90ded38a490806f943f1de6b0e409512
Martin Dinh <martin@dinhmail.de>
2012-06-24 22:07:07 +0200

    implemented FindPath algorithm with small test case

--- implementation/code/src/de/graphioli/algorithms/FindPath.java
+++ implementation/code/src/de/graphioli/algorithms/FindPath.java
@@ -1,5 +1,43 @@
 package de.graphioli.algorithms;
 
-public class FindPath {
+import java.util.ArrayList;
 
+import de.graphioli.model.Graph;
+import de.graphioli.model.Vertex;
+
+/**
+ * This class can check if a path between two given vertices exists.
+ * 
+ * @author Graphioli
+ * 
+ */
+public final class FindPath {
+
+	/**
+	 * Private empty constructor, to ensure that no instance is being created.
+	 */
+	private FindPath() {
+	}
+
+	/**
+	 * Checks if a path exists in a specified graph between two given vertices.
+	 * This method uses the {@link BreadthFirstSearch} algorithm.
+	 * 
+	 * @param graph
+	 *            specified graph
+	 * @param vertexA
+	 *            starting {@link Vertex}
+	 * @param vertexB
+	 *            {@link Vertex} to check for
+	 * @return <code>true</code> if a path exists between the two given
+	 *         vertices, <code>false</code> otherwise.
+	 */
+	public static boolean performAlgorithm(Graph graph, Vertex vertexA, Vertex vertexB) {
+		ArrayList<Vertex> reachableVertices = BreadthFirstSearch.performAlgorithm(graph, vertexA, 1000);
+		if (reachableVertices.contains(vertexB)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
