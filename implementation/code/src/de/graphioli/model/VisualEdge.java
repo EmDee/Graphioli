4d93b5c41596f2cb6ea4934fbb8c2f7a246324c7
Jack Unseld <jackunseld@aol.com>
2012-06-25 11:32:00 +0200

    added missing methods

--- implementation/code/src/de/graphioli/model/VisualEdge.java
+++ implementation/code/src/de/graphioli/model/VisualEdge.java
@@ -1,10 +1,17 @@
 package de.graphioli.model;
 
+import java.awt.Image;
+
 public abstract class VisualEdge extends Edge {
 
 	public VisualEdge(Vertex vertexA, Vertex vertexB) {
 		super(vertexA, vertexB);
 		// TODO Auto-generated constructor stub
+	}
+
+	public Image getBufferedImage() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
