e8fa75fb8cafa5c706301718c6f98724dd463413
Jack Unseld <jackunseld@aol.com>
2012-06-25 11:27:09 +0200

    implemented display of graph (not tested)

--- implementation/code/src/de/graphioli/view/GraphCanvas.java
+++ implementation/code/src/de/graphioli/view/GraphCanvas.java
@@ -1,4 +1,13 @@
 package de.graphioli.view;
+import de.graphioli.model.Edge;
+import de.graphioli.model.Graph;
+import de.graphioli.model.Vertex;
+import de.graphioli.model.VisualEdge;
+import de.graphioli.model.VisualVertex;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class GraphCanvas extends JPanel {
@@ -11,6 +20,7 @@ public class GraphCanvas extends JPanel {
 	/**
 	 * The {@link VisualGrid} associated with this {@link GraphCanvas}
 	 */
+	private VisualGrid visualGrid;
 	
 	/**
 	 * Creates a {@link GraphCanvas} and registers its parent {@link GameWindow}.
@@ -18,7 +28,9 @@ public class GraphCanvas extends JPanel {
 	 * @param parentGameWindow The {@link GameWindow} that contains this {@link GraphCanvas}
 	 */
 	public GraphCanvas(GameWindow parentGameWindow) {
-		
+		this.parentGameWindow = parentGameWindow;
+		visualGrid = new VisualGrid(parentGameWindow);
+		this.addMouseListener(visualGrid);
 	}
 	
 	/**
@@ -27,7 +39,68 @@ public class GraphCanvas extends JPanel {
 	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
 	 */
 	public boolean updateCanvas() {
+		paintComponent(null);
 		return true;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		Graph graph = parentGameWindow.getViewManager().getGameManager().getGameBoard().getGraph();
+		int horizontalGridScale = (int) (this.getHeight() / (visualGrid.getGrid().getVerticalGridPoints() + 1));
+		int verticalGridScale = (int) (this.getWidth() / (visualGrid.getGrid().getHorizontalGridPoints() + 1));
+		
+		for(Vertex v: graph.getVertices()) {
+			/*
+			 * on grid
+			 */
+			VisualVertex vertex = (VisualVertex) v;
+			g2d.drawImage(vertex.getBufferedImage(),
+					(vertex.getGridPoint().getPositionX() * horizontalGridScale) - (visualGrid.getVisualVertexSize() / 2),
+					(vertex.getGridPoint().getPositionY() * verticalGridScale) - (visualGrid.getVisualVertexSize() / 2),
+					visualGrid.getVisualVertexSize(),
+					visualGrid.getVisualVertexSize(),
+					null);
+			
+		}
+		
+		for(Edge edge: graph.getEdges()) {
+			/*
+			 * Size of the VisualEdge bzgl. grid
+			 */
+			int xsize = 0;
+			int ysize = 0;
+
+			/*
+			 * Positioning of the VisualEdge on grid
+			 */
+			int xpos = 0;
+			int ypos = 0;
+				
+			VisualVertex originVertex = (VisualVertex) edge.getOriginVertex();
+			VisualVertex targetVertex = (VisualVertex) edge.getTargetVertex();
+			
+			if((originVertex.getGridPoint().getPositionX() > targetVertex.getGridPoint().getPositionX())) {
+				xsize = (originVertex.getGridPoint().getPositionX() - targetVertex.getGridPoint().getPositionX()) * horizontalGridScale;
+				xpos = (targetVertex.getGridPoint().getPositionX() * horizontalGridScale);
+			} else {
+				xsize = (targetVertex.getGridPoint().getPositionX() - originVertex.getGridPoint().getPositionX()) * horizontalGridScale;
+				xpos = (originVertex.getGridPoint().getPositionX() * horizontalGridScale);
+			}
+			if((originVertex.getGridPoint().getPositionY() > targetVertex.getGridPoint().getPositionY())) {
+				ysize = (originVertex.getGridPoint().getPositionY() - targetVertex.getGridPoint().getPositionY()) * verticalGridScale;
+				xpos = (targetVertex.getGridPoint().getPositionY() * verticalGridScale);
+			} else {
+				ysize = (targetVertex.getGridPoint().getPositionY() - originVertex.getGridPoint().getPositionY()) * verticalGridScale;
+				xpos = (originVertex.getGridPoint().getPositionY() * verticalGridScale);
+			}
+					
+			g2d.drawImage(((VisualEdge) edge).getBufferedImage(), xpos, ypos, xsize, ysize, null);
+			
+		}
 		
 	}
 }
