0638df83dd40715bfa1cacb7e0bbdc5d84e49a4a
Johannes Schäfer <github@johannes-schaefer.net>
2012-06-25 14:23:50 +0200

    Implemented interface GEView.

--- implementation/code/src/de/graphioli/gameexplorer/GEView.java
+++ implementation/code/src/de/graphioli/gameexplorer/GEView.java
@@ -1,5 +1,27 @@
 package de.graphioli.gameexplorer;
 
+/**
+ * This interface defines the methods the {@link GameExplorer} uses to
+ * communicate with its graphical user interface (GUI).
+ * 
+ * @author Graphioli
+ */
 public interface GEView {
+
+	/**
+	 * Registers the controller for the {@link GameExplorer} user interface.
+	 * 
+	 * @param gameExplorer The controlling GameExplorer
+	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
+	 */
+	boolean registerController(GameExplorer gameExplorer);
+
+
+	/**
+	 * Generates the user interface of the {@link GameExplorer}.
+	 * 
+	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
+	 */
+	boolean generateView();
 
 }
