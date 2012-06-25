9aede1b4c2e5236dad254a33e26d5514c3a98ed2
Ole Fortmann <o.fortmann@hotmail.de>
2012-06-25 16:45:39 +0200

    added visibility attribute to visual edge

--- implementation/code/src/de/graphioli/model/VisualEdge.java
+++ implementation/code/src/de/graphioli/model/VisualEdge.java
@@ -3,15 +3,26 @@ package de.graphioli.model;
 import java.awt.Image;
 
 public abstract class VisualEdge extends Edge {
+	
+	private boolean visible;
 
 	public VisualEdge(Vertex vertexA, Vertex vertexB) {
 		super(vertexA, vertexB);
+		this.visible = true;
 		// TODO Auto-generated constructor stub
 	}
 
 	public Image getBufferedImage() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public boolean isVisible() {
+		return this.visible;
+	}
+	
+	public void setVisible(boolean visible) {
+		this.visible = visible;
 	}
 
 }
