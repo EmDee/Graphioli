eed2cd63ba43fedfd1abdc213ec9d0535a507536
Jack Unseld <jackunseld@aol.com>
2012-06-26 18:41:48 +0200

    added displaying of the grid

--- implementation/code/src/de/graphioli/view/VisualGrid.java
+++ implementation/code/src/de/graphioli/view/VisualGrid.java
@@ -4,11 +4,13 @@ import de.graphioli.model.Grid;
 import de.graphioli.model.GridPoint;
 import de.graphioli.model.VisualVertex;
 
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * This class handles mouse input, serves as a connector between {@link Grid} and display and handles the size of the drawn {@link VisualVertex}.
+ * This class handles mouse input, serves as a connector between {@link Grid}
+ * and display and handles the size of the drawn {@link VisualVertex}.
  * 
  * @author Graphioli
  */
@@ -18,52 +20,61 @@ public class VisualGrid implements MouseListener {
 	 * The parent {@link GameWindow} associated with this {@link VisualGrid}
 	 */
 	private GameWindow parentGameWindow;
-	
+
 	/**
 	 * The {@link GraphCanvas} associated with this {@link VisualGrid}
 	 */
 	private GraphCanvas graphCanvas;
-	
+
 	/**
 	 * The {@link Grid} associated with this {@link VisualGrid}
 	 */
 	private Grid grid;
-	
+
 	/**
-	 * The size of the {@link VisualVertex}es displayed in the {@link GraphCanvas}
+	 * The size of the {@link VisualVertex}es displayed in the
+	 * {@link GraphCanvas}
 	 */
 	private int visualVertexSize;
-	
+
 	/**
 	 * The distance of two {@link GridPoint}s in the {@link GraphCanvas}
 	 */
 	private int gridScale;
-	
+
 	/**
 	 * Creates a {@link VisualGrid} and registers its parent {@link GameWindow}.
 	 * 
-	 * @param parentGameWindow The {@link GameWindow} that contains the {@link GraphCanvas} to this {@link VisualGrid}
+	 * @param parentGameWindow
+	 *            The {@link GameWindow} that contains the {@link GraphCanvas}
+	 *            to this {@link VisualGrid}
 	 */
 	public VisualGrid(GraphCanvas graphCanvas, GameWindow parentGameWindow) {
 		this.parentGameWindow = parentGameWindow;
 		this.graphCanvas = graphCanvas;
-		this.grid = this.parentGameWindow.getViewManager().getGameManager().getGameBoard().getGrid();
+		this.grid = this.parentGameWindow.getViewManager().getGameManager()
+				.getGameBoard().getGrid();
 		// default gridScale based on VertexSize for now
 		this.gridScale = (int) (VisualVertex.PIXELS_PER_SIDE * 1.8);
 	}
-	
-	
+
 	/**
-	 * Parses the coordinates of the mouse click to the specific {@link GridPoint}.
+	 * Parses the coordinates of the mouse click to the specific
+	 * {@link GridPoint}.
 	 * 
-	 * @param xCoord The x coordinate of the mouse click
-	 * @param yCoord The y coordinate of the mouse click
-	 * @return GridPoint The {@link GridPoint} to the responsible mouse click coordinates
+	 * @param xCoord
+	 *            The x coordinate of the mouse click
+	 * @param yCoord
+	 *            The y coordinate of the mouse click
+	 * @return GridPoint The {@link GridPoint} to the responsible mouse click
+	 *         coordinates
 	 */
 	public GridPoint parseCoordinates(int xCoord, int yCoord) {
-		int xpos = Math.round((xCoord - VisualVertex.PIXELS_PER_SIDE / 2) / this.gridScale) + 1;
-		int ypos = Math.round((yCoord - VisualVertex.PIXELS_PER_SIDE / 2) / this.gridScale) + 1;
-		if (((xpos * this.gridScale + VisualVertex.PIXELS_PER_SIDE / 2) > xCoord) 
+		int xpos = Math.round((xCoord - VisualVertex.PIXELS_PER_SIDE / 2)
+				/ this.gridScale) + 1;
+		int ypos = Math.round((yCoord - VisualVertex.PIXELS_PER_SIDE / 2)
+				/ this.gridScale) + 1;
+		if (((xpos * this.gridScale + VisualVertex.PIXELS_PER_SIDE / 2) > xCoord)
 				&& ((xpos * this.gridScale - VisualVertex.PIXELS_PER_SIDE / 2) < xCoord)
 				&& ((ypos * this.gridScale + VisualVertex.PIXELS_PER_SIDE / 2) > yCoord)
 				&& ((ypos * this.gridScale - VisualVertex.PIXELS_PER_SIDE / 2) < yCoord)) {
@@ -73,16 +84,38 @@ public class VisualGrid implements MouseListener {
 	}
 
 	/**
-	 * Sets the size of the displayed {@link VisualVertex}es up to a {@link Grid} specific maximum value.
+	 * Draws the grid on the given Graphics2D Object.
 	 * 
-	 * @param size The size of the vertices
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
+	 * @param g2d
+	 */
+	public void draw(Graphics2D g2d) {
+		// Drawing grid
+		for (int xPoints = 0; xPoints < this.grid
+				.getHorizontalGridPoints(); xPoints++) {
+			g2d.drawLine((xPoints + 1) * gridScale, 0, (xPoints + 1)
+					* gridScale, this.graphCanvas.getHeight());
+		}
+		for (int yPoints = 0; yPoints < this.grid
+				.getVerticalGridPoints(); yPoints++) {
+			g2d.drawLine(0, (yPoints + 1) * gridScale, this.graphCanvas.getWidth(),
+					(yPoints + 1) * gridScale);
+		}
+	}
+
+	/**
+	 * Sets the size of the displayed {@link VisualVertex}es up to a
+	 * {@link Grid} specific maximum value.
+	 * 
+	 * @param size
+	 *            The size of the vertices
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise
 	 */
 	public boolean setVisualVertexSize(int size) {
 		this.visualVertexSize = size;
 		return true;
 	}
-	
+
 	/**
 	 * Returns the size of the displayed {@link VisualVertex}es.
 	 * 
@@ -91,13 +124,13 @@ public class VisualGrid implements MouseListener {
 	public int getVisualVertexSize() {
 		return this.visualVertexSize;
 	}
-	
+
 	/**
 	 * Returns the {@link Grid} associated with the {@link VisualGrid}.
 	 * 
-	 * @return Grid The  of this
+	 * @return Grid The of this
 	 */
-	public Grid getGrid() {	
+	public Grid getGrid() {
 		return this.grid;
 	}
 
@@ -111,15 +144,21 @@ public class VisualGrid implements MouseListener {
 	}
 
 	/**
-	 * Invoked if the mouse button has been clicked, calls parseCoordinates(int xCoord, int yCoord) and forwards the selected {@link GridPoint} to its parent {@link GameWindow}.
+	 * Invoked if the mouse button has been clicked, calls parseCoordinates(int
+	 * xCoord, int yCoord) and forwards the selected {@link GridPoint} to its
+	 * parent {@link GameWindow}.
 	 * 
-	 * @param event The {@link MouseEvent} containing the coordinates of the mouse click
+	 * @param event
+	 *            The {@link MouseEvent} containing the coordinates of the mouse
+	 *            click
 	 */
 	@Override
 	public void mouseClicked(MouseEvent event) {
-		GridPoint gridPointClicked = this.parseCoordinates(event.getX(), event.getY());
+		GridPoint gridPointClicked = this.parseCoordinates(event.getX(),
+				event.getY());
 		if (gridPointClicked != null) {
-			this.parentGameWindow.getViewManager().onGridPointClick(gridPointClicked); 
+			this.parentGameWindow.getViewManager().onGridPointClick(
+					gridPointClicked);
 		}
 	}
 
