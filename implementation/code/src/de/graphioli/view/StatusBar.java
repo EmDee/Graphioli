e7ac5dba496af5133a89c22aa7b49742fc8649e4
Leon Hielscher <Leon@dr-hielscher.de>
2012-06-26 15:24:48 +0200

    Implemented StatusBar.

--- implementation/code/src/de/graphioli/view/StatusBar.java
+++ implementation/code/src/de/graphioli/view/StatusBar.java
@@ -1,7 +1,17 @@
 package de.graphioli.view;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.logging.Logger;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
 
 import de.graphioli.model.Player;
+import de.graphioli.model.VisualVertex;
 
 /**
  * The StatusBar displays which Player is currently active and a short status or error message.
@@ -9,6 +19,12 @@ import de.graphioli.model.Player;
  * @author Graphioli
  */
 public class StatusBar extends JPanel {
+	
+	/**
+	 * Logging instance
+	 */
+	private final static Logger LOG = Logger.getLogger(StatusBar.class
+			.getName());
 	
 	/**
 	 * The error message that is currently displayed
@@ -19,11 +35,26 @@ public class StatusBar extends JPanel {
 	 * The {@link Player} that is currently displayed as active
 	 */
 	private Player playerStatus;
+	
+	private JTextField statusLabel;
+	private JTextField playerLabel;
 
 	/**
 	 * Constructs a {@link StatusBar} responsible for displaying the playerStatus and errorMessages
 	 */
 	public StatusBar() {
+		LOG.fine("StatusBar created.");
+		this.setLayout(new BorderLayout());
+		this.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		statusLabel = new JTextField("status");
+		statusLabel.setEditable(false);
+		statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		add(statusLabel, BorderLayout.WEST);
+		playerLabel = new JTextField("player");
+		playerLabel.setEditable(false);
+		playerLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(playerLabel, BorderLayout.EAST);
 	}
 	
 	/**
@@ -33,6 +64,8 @@ public class StatusBar extends JPanel {
 	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
 	 */
 	public boolean updatePlayerStatus(Player player) {
+		this.playerStatus = player;
+		playerLabel.setText(player.getName());
 		return true;
 	}
 	
@@ -43,6 +76,8 @@ public class StatusBar extends JPanel {
 	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
 	 */
 	public boolean displayErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+		statusLabel.setText(errorMessage);
 		return true;
 	}
 	
