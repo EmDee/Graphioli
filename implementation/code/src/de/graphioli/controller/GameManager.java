a4cf5b6b0484931349840f1aaa6c380c4cb0be3b
Martin Dinh <martin@dinhmail.de>
2012-06-27 11:28:24 +0200

    moved openHelpFile to GameManager

--- implementation/code/src/de/graphioli/controller/GameManager.java
+++ implementation/code/src/de/graphioli/controller/GameManager.java
@@ -16,8 +16,9 @@ import de.graphioli.model.Player;
 import de.graphioli.utils.GraphioliLogger;
 
 /**
- * This is the framework's central class, connecting the actual game with the model and the view.
- * Any communication between model, view and controller will be managed by the GameManager.
+ * This is the framework's central class, connecting the actual game with the
+ * model and the view. Any communication between model, view and controller will
+ * be managed by the GameManager.
  * 
  * @author Graphioli
  */
@@ -53,14 +54,19 @@ public class GameManager {
 	 */
 	private String gamePackagePath = "game.";
 
+	/**
+	 * GameDefinition for the current played game.
+	 */
+	private GameDefinition currentGameDefinition;
 
 	/**
 	 * Initializes the GameManager to start the whole application.
 	 * 
-	 * @param args Provided command-line arguments
+	 * @param args
+	 *            Provided command-line arguments
 	 */
 	public static void main(String[] args) {
-		
+
 		// Start Logging
 		if (!initLogger()) {
 			System.out.print("STOP: Unable to create log file.");
@@ -73,11 +79,10 @@ public class GameManager {
 		// Open GameExplorer to select a game
 		// This will cause GameExplorer to call startGame()
 		gameManager.openGameExplorer();
-		
+
 		LOG.fine("<em>main</em> method finished");
 
 	}
-
 
 	/**
 	 * Creates a new instance of {@link GameManager}.
@@ -86,11 +91,11 @@ public class GameManager {
 		LOG.info("GameManager instantiated.");
 	}
 
-
 	/**
 	 * Starts the {@link GameExplorer}.
 	 * 
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise
 	 */
 	private boolean openGameExplorer() {
 
@@ -103,24 +108,25 @@ public class GameManager {
 
 	}
 
-
 	/**
 	 * Kills the currently running game.
 	 * 
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO: Implement
 	 */
 	private boolean killGame() {
 		return false;
 	}
 
-
 	/**
 	 * Starts the game specified by the {@link GameDefinition}.
 	 * 
-	 * @param gameDefinition The GameDefinition of the game to start
-	 * @param players The list of players for this game
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
+	 * @param gameDefinition
+	 *            The GameDefinition of the game to start
+	 * @param players
+	 *            The list of players for this game
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise
 	 */
 	public boolean startGame(GameDefinition gameDefinition, ArrayList<Player> players) {
 
@@ -131,11 +137,12 @@ public class GameManager {
 
 		// Create GameBoard
 		this.gameBoard = new GameBoard(gameDefinition.isDirectedGraph(),
-				gameDefinition.getHorizontalGridPointCount(),
-				gameDefinition.getVerticalGridPointCount());
+				gameDefinition.getHorizontalGridPointCount(), gameDefinition.getVerticalGridPointCount());
 
 		// Create ViewManager instance
 		this.viewManager = new ViewManager(this);
+
+		this.currentGameDefinition = gameDefinition;
 
 		// Facultative: Create MenuItems here
 
@@ -153,7 +160,8 @@ public class GameManager {
 			}
 
 			// Create jarBall file pointer to specific game class
-			// TODO Get path (currently /src/games/) from environment (eg. production and development)
+			// TODO Get path (currently /src/games/) from environment (eg.
+			// production and development)
 			File jarBall = new File(canonicalPath + "/src/games/" + gameDefinition.getClassName() + "/");
 			URL[] urls = new URL[1];
 
@@ -187,99 +195,94 @@ public class GameManager {
 
 		game.registerController(this);
 
-		
 		// Start Game
-		
+
 		LOG.finer("Calling <em>onGameInit()</em>.");
-		
+
 		if (game.onGameInit()) {
 			LOG.fine("<em>onGameInit()</em> returned <em>true</em>.");
-			
+
 			LOG.finer("Calling <em>onGameStart()</em>.");
-			
+
 			if (game.onGameStart()) {
 				LOG.info("<em>onGameStart()</em> returned <em>true</em>. Game started.");
 			} else {
 				LOG.warning("<em>onGameStart()</em> returned <em>false</em>.");
 			}
-			
+
 		} else {
 			LOG.warning("<em>onGameInit()</em> returned <em>false</em>.");
 		}
-		
-		
+
 		return true;
 
 	}
-
 
 	/**
 	 * Creates a {@link GameCapsule} from the savegame file and loads the
 	 * information to start the game in the saved state.
 	 * 
-	 * @param savegame The savegame file
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @param savegame
+	 *            The savegame file
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO: Implement
 	 */
 	public boolean loadGame(File savegame) {
 		return false;
 	}
 
-
 	/**
-	 * Creates a {@link GameCapsule} and serializes it into a savegame file
-	 * to save the current state of the game.
+	 * Creates a {@link GameCapsule} and serializes it into a savegame file to
+	 * save the current state of the game.
 	 * 
-	 * @param savegame The savegame file
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @param savegame
+	 *            The savegame file
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO Implement
 	 */
 	public boolean saveGame(File savegame) {
 		return false;
 	}
 
-
 	/**
 	 * Finishes the game and displays a default pop-up for single player games.
 	 * 
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO Implement
 	 */
 	public boolean finishGame() {
 		this.killGame();
 		return false;
 	}
 
-
 	/**
 	 * Finishes the game and displays the winning {@link Player} in a pop-up.
 	 * 
-	 * @param winner The winning player
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @param winner
+	 *            The winning player
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO Implement
 	 */
 	public boolean finishGame(Player winner) {
 		return false;
 	}
 
-
 	/**
 	 * Restarts the game and resets the {@link GameBoard} and {@link Player}s.
 	 * 
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO Implement
 	 */
 	public boolean restartGame() {
 		return false;
 	}
 
-
 	/**
-	 * Closes the game with its {@link GameWindow} and returns the focus to
-	 * the {@link GameExplorer}.
+	 * Closes the game with its {@link GameWindow} and returns the focus to the
+	 * {@link GameExplorer}.
 	 * 
-	 * @return <code>true</code> if the action was performed successfully, <code>false</code> otherwise
-	 * @todo Implement
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise TODO Implement
 	 */
 	public boolean closeGame() {
 		this.viewManager.closeView();
@@ -287,16 +290,15 @@ public class GameManager {
 		return true;
 	}
 
-
 	/**
-	 * Returns the {@link PlayerManager} associated with this {@link GameManager}.
+	 * Returns the {@link PlayerManager} associated with this
+	 * {@link GameManager}.
 	 * 
 	 * @return PlayerManager The PlayerManager
 	 */
 	public PlayerManager getPlayerManager() {
 		return this.playerManager;
 	}
-
 
 	/**
 	 * Returns the {@link ViewManager} associated with this {@link GameManager}.
@@ -307,7 +309,6 @@ public class GameManager {
 		return this.viewManager;
 	}
 
-
 	/**
 	 * Returns the {@link GameBoard} associated with this {@link GameManager}.
 	 * 
@@ -316,7 +317,7 @@ public class GameManager {
 	public GameBoard getGameBoard() {
 		return this.gameBoard;
 	}
-	
+
 	/**
 	 * Returns the {@link Game} associated with this {@link GameManager}.
 	 * 
@@ -325,7 +326,7 @@ public class GameManager {
 	public Game getGame() {
 		return this.game;
 	}
-	
+
 	/**
 	 * Starts the logging for the framework.
 	 * 
@@ -341,6 +342,40 @@ public class GameManager {
 
 		return true;
 
+	}
+
+	/**
+	 * Opens the help file with the link in the given {@link GameDefinition}.
+	 * 
+	 * @param gameDefinition
+	 *            given {@link GameDefinition}
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise
+	 */
+	public boolean openHelpFile(GameDefinition gameDefinition) {
+		LOG.info("GameManager.<em>openHelpFile([...])</em> called.");
+
+		// TODO: getDesktop().browse only works for Java 6
+
+		String url = gameDefinition.getHelpFile().toString();
+		try {
+			java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+		} catch (IOException e) {
+			LOG.severe("IOException: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	/**
+	 * Opens the help file with the link of the current {@link GameDefinition}.
+	 * 
+	 * @return <code>true</code> if the action was performed successfully,
+	 *         <code>false</code> otherwise
+	 */
+	public boolean openHelpFile() {
+		return this.openHelpFile(this.currentGameDefinition);
 	}
 
 }
