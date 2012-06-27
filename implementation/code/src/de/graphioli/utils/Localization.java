4ecc4c5f350a44b349a3e72f319b09e63c29242c
Martin Dinh <martin@dinhmail.de>
2012-06-27 15:05:57 +0200

    added javadoc and encoded language file

--- implementation/code/src/de/graphioli/utils/Localization.java
+++ implementation/code/src/de/graphioli/utils/Localization.java
@@ -4,8 +4,28 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class Localization {
+/**
+ * 
+ * This class is responsible for loading the correct localization file.
+ * 
+ * @author Graphioli
+ * 
+ */
+public final class Localization {
+	// TODO: load localization from game definition
+	/**
+	 * Private constructor to ensure that no instance of this class is created.
+	 */
+	private Localization() {
+	}
 
+	/**
+	 * Gets the respective string to a given key value.
+	 * 
+	 * @param key
+	 *            given key
+	 * @return the string that corresponds to the key
+	 */
 	public static String getLanguageString(String key) {
 
 		//
@@ -13,7 +33,7 @@ public class Localization {
 		// bundle will load the MessagesBundle_en_GB.properties file.
 		//
 		try {
-			ResourceBundle bundle = ResourceBundle.getBundle("lang", new Locale("en", "US"));
+			ResourceBundle bundle = ResourceBundle.getBundle("lang", new Locale("de", "DE"));
 			return bundle.getString(key);
 		} catch (MissingResourceException mre) {
 			System.out.println("Can't find locale file");
