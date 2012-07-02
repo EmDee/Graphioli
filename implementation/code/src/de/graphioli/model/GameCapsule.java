13de8f69e3f3c255925942c44702950c63bbb246
Ole Fortmann <o.fortmann@hotmail.de>
2012-07-02 14:54:26 +0200

    added javaDoc to GameCapsule

--- implementation/code/src/de/graphioli/model/GameCapsule.java
+++ implementation/code/src/de/graphioli/model/GameCapsule.java
@@ -11,54 +11,60 @@ import java.util.ArrayList;
  * @author Graphioli
  */
 public class GameCapsule implements Serializable {
-	
+
 	/**
 	 * The game board in the state to be saved
 	 */
 	private GameBoard board;
-	
+
 	/**
 	 * The players of the game to be saved
 	 */
 	private ArrayList<Player> players;
-	
+
 	/**
 	 * The currently active player
 	 */
 	private Player activePlayer;
-	
+
 	/**
 	 * Creates a new GameCapsule with the given parameters.
 	 * 
 	 * @param board
+	 *            the game board in the state to be saved
 	 * @param players
+	 *            the players of the game to be saved
 	 * @param activePlayer
+	 *            the currently active player
 	 */
 	public GameCapsule(GameBoard board, ArrayList<Player> players, Player activePlayer) {
 		this.board = board;
 		this.players = players;
 		this.activePlayer = activePlayer;
 	}
-	
+
 	/**
+	 * Returns the saved {@link GameBoard}.
 	 * 
-	 * @return
+	 * @return the saved {@link GameBoard}
 	 */
 	public GameBoard getBoard() {
 		return this.board;
 	}
-	
+
 	/**
+	 * Returns the saved {@link Player}s.
 	 * 
-	 * @return
+	 * @return an ArrayList of the saved {@link Player}s
 	 */
 	public ArrayList<Player> getPlayers() {
 		return this.players;
 	}
-	
+
 	/**
+	 * Returns the saved active {@link Player}.
 	 * 
-	 * @return
+	 * @return the saved acrive {@link Player}
 	 */
 	public Player getActivePlayer() {
 		return this.activePlayer;
