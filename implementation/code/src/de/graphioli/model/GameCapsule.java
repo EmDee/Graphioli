8fcdff01aca3afc2b32cbbe7046db7a6d9f3c23a
Ole Fortmann <o.fortmann@hotmail.de>
2012-07-02 12:02:37 +0200

    implemented GameCapsule class

--- implementation/code/src/de/graphioli/model/GameCapsule.java
+++ implementation/code/src/de/graphioli/model/GameCapsule.java
@@ -1,6 +1,7 @@
 package de.graphioli.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * This serializable class represents a GameCapsule that holds all the important
@@ -10,5 +11,56 @@ import java.io.Serializable;
  * @author Graphioli
  */
 public class GameCapsule implements Serializable {
-
+	
+	/**
+	 * The game board in the state to be saved
+	 */
+	private GameBoard board;
+	
+	/**
+	 * The players of the game to be saved
+	 */
+	private ArrayList<Player> players;
+	
+	/**
+	 * The currently active player
+	 */
+	private Player activePlayer;
+	
+	/**
+	 * Creates a new GameCapsule with the given parameters.
+	 * 
+	 * @param board
+	 * @param players
+	 * @param activePlayer
+	 */
+	public GameCapsule(GameBoard board, ArrayList<Player> players, Player activePlayer) {
+		this.board = board;
+		this.players = players;
+		this.activePlayer = activePlayer;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public GameBoard getBoard() {
+		return this.board;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Player> getPlayers() {
+		return this.players;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Player getActivePlayer() {
+		return this.activePlayer;
+	}
 }
