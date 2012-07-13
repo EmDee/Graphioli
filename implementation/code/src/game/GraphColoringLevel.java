ecfeaa85a57b30820d5d4cf29373f083d1e8acc6
Ole Fortmann <o.fortmann@hotmail.de>
2012-07-14 01:34:58 +0200

    added new GraphColoring level (6)

--- implementation/code/src/game/GraphColoringLevel.java
+++ implementation/code/src/game/GraphColoringLevel.java
@@ -14,7 +14,7 @@ public final class GraphColoringLevel {
 	/*
 	 * the number of levels that are implemented
 	 */
-	public static final int IMPLEMENTED_LEVEL_COUNT = 5;
+	public static final int IMPLEMENTED_LEVEL_COUNT = 6;
 
 	/**
 	 * The number of colors to color the graph.
@@ -66,6 +66,8 @@ public final class GraphColoringLevel {
 				return generateLevelFour();
 			case 5:
 				return generateLevelFive();
+			case 6:
+				return generateLevelSix();
 			default:
 				return null;
 		}
@@ -315,6 +317,93 @@ public final class GraphColoringLevel {
 		edges[73] = new SimpleVisualEdge(vertices[13], vertices[24]);
 
 		return new GraphColoringLevel(4, vertices, edges);
+	}
+	
+	private static GraphColoringLevel generateLevelSix() {
+		GraphColoringVertex[] vertices = new GraphColoringVertex[26];
+		VisualEdge[] edges = new VisualEdge[52]; 
+		
+		vertices[0] = new GraphColoringVertex(new GridPoint(4, 1));
+		vertices[1] = new GraphColoringVertex(new GridPoint(6, 1));
+		vertices[2] = new GraphColoringVertex(new GridPoint(1, 2));
+		vertices[3] = new GraphColoringVertex(new GridPoint(9, 2));
+		vertices[4] = new GraphColoringVertex(new GridPoint(2, 4));
+		vertices[5] = new GraphColoringVertex(new GridPoint(5, 4));
+		vertices[6] = new GraphColoringVertex(new GridPoint(8, 4));
+		vertices[7] = new GraphColoringVertex(new GridPoint(1, 5));
+		vertices[8] = new GraphColoringVertex(new GridPoint(4, 5));
+		vertices[9] = new GraphColoringVertex(new GridPoint(6, 5));
+		vertices[10] = new GraphColoringVertex(new GridPoint(9, 5));
+		vertices[11] = new GraphColoringVertex(new GridPoint(0, 6));
+		vertices[12] = new GraphColoringVertex(new GridPoint(3, 6));
+		vertices[13] = new GraphColoringVertex(new GridPoint(7, 6));
+		vertices[14] = new GraphColoringVertex(new GridPoint(10, 6));
+		vertices[15] = new GraphColoringVertex(new GridPoint(4, 7));
+		vertices[16] = new GraphColoringVertex(new GridPoint(6, 7));
+		vertices[17] = new GraphColoringVertex(new GridPoint(2, 8));
+		vertices[18] = new GraphColoringVertex(new GridPoint(4, 8));
+		vertices[19] = new GraphColoringVertex(new GridPoint(6, 8));
+		vertices[20] = new GraphColoringVertex(new GridPoint(8, 8));
+		vertices[21] = new GraphColoringVertex(new GridPoint(3, 10));
+		vertices[22] = new GraphColoringVertex(new GridPoint(5, 10));
+		vertices[23] = new GraphColoringVertex(new GridPoint(7, 10));
+		vertices[24] = new GraphColoringVertex(new GridPoint(2, 11));
+		vertices[25] = new GraphColoringVertex(new GridPoint(8, 11));
+		
+		edges[0] = new SimpleVisualEdge(vertices[0], vertices[5]);
+		edges[1] = new SimpleVisualEdge(vertices[1], vertices[5]);
+		edges[2] = new SimpleVisualEdge(vertices[2], vertices[4]);
+		edges[3] = new SimpleVisualEdge(vertices[2], vertices[5]);
+		edges[4] = new SimpleVisualEdge(vertices[2], vertices[7]);
+		edges[5] = new SimpleVisualEdge(vertices[2], vertices[11]);
+		edges[6] = new SimpleVisualEdge(vertices[3], vertices[5]);
+		edges[7] = new SimpleVisualEdge(vertices[3], vertices[6]);
+		edges[8] = new SimpleVisualEdge(vertices[3], vertices[9]);
+		edges[9] = new SimpleVisualEdge(vertices[3], vertices[14]);
+		edges[10] = new SimpleVisualEdge(vertices[4], vertices[7]);
+		edges[11] = new SimpleVisualEdge(vertices[4], vertices[8]);
+		edges[12] = new SimpleVisualEdge(vertices[5], vertices[8]);
+		edges[13] = new SimpleVisualEdge(vertices[5], vertices[9]);
+		edges[14] = new SimpleVisualEdge(vertices[5], vertices[15]);
+		edges[15] = new SimpleVisualEdge(vertices[5], vertices[16]);
+		edges[16] = new SimpleVisualEdge(vertices[6], vertices[9]);
+		edges[17] = new SimpleVisualEdge(vertices[6], vertices[10]);
+		edges[18] = new SimpleVisualEdge(vertices[6], vertices[13]);
+		edges[19] = new SimpleVisualEdge(vertices[7], vertices[11]);
+		edges[20] = new SimpleVisualEdge(vertices[7], vertices[12]);
+		edges[21] = new SimpleVisualEdge(vertices[8], vertices[12]);
+		edges[22] = new SimpleVisualEdge(vertices[8], vertices[15]);
+		edges[23] = new SimpleVisualEdge(vertices[9], vertices[13]);
+		edges[24] = new SimpleVisualEdge(vertices[10], vertices[13]);
+		edges[25] = new SimpleVisualEdge(vertices[10], vertices[14]);
+		edges[26] = new SimpleVisualEdge(vertices[11], vertices[15]);
+		edges[27] = new SimpleVisualEdge(vertices[12], vertices[15]);
+		edges[28] = new SimpleVisualEdge(vertices[13], vertices[16]);
+		edges[29] = new SimpleVisualEdge(vertices[14], vertices[16]);
+		edges[30] = new SimpleVisualEdge(vertices[15], vertices[16]);
+		edges[31] = new SimpleVisualEdge(vertices[15], vertices[17]);
+		edges[32] = new SimpleVisualEdge(vertices[15], vertices[22]);
+		edges[33] = new SimpleVisualEdge(vertices[16], vertices[19]);
+		edges[34] = new SimpleVisualEdge(vertices[16], vertices[20]);
+		edges[35] = new SimpleVisualEdge(vertices[16], vertices[22]);
+		edges[36] = new SimpleVisualEdge(vertices[17], vertices[18]);
+		edges[37] = new SimpleVisualEdge(vertices[17], vertices[21]);
+		edges[38] = new SimpleVisualEdge(vertices[17], vertices[22]);
+		edges[39] = new SimpleVisualEdge(vertices[17], vertices[24]);
+		edges[40] = new SimpleVisualEdge(vertices[18], vertices[21]);
+		edges[41] = new SimpleVisualEdge(vertices[18], vertices[22]);
+		edges[42] = new SimpleVisualEdge(vertices[19], vertices[20]);
+		edges[43] = new SimpleVisualEdge(vertices[19], vertices[22]);
+		edges[44] = new SimpleVisualEdge(vertices[19], vertices[23]);
+		edges[45] = new SimpleVisualEdge(vertices[20], vertices[23]);
+		edges[46] = new SimpleVisualEdge(vertices[20], vertices[25]);
+		edges[47] = new SimpleVisualEdge(vertices[22], vertices[23]);
+		edges[48] = new SimpleVisualEdge(vertices[22], vertices[24]);
+		edges[49] = new SimpleVisualEdge(vertices[22], vertices[25]);
+		edges[50] = new SimpleVisualEdge(vertices[23], vertices[25]);
+		edges[51] = new SimpleVisualEdge(vertices[13], vertices[14]);
+		
+		return new GraphColoringLevel(3, vertices, edges);
 	}
 
 	/**
